f731a460ea4f448bdf4633829e3c18201da983fd
Nico <96704103+PanterSoft@users.noreply.github.com>
2025-01-10 23:03:09 +0100

    Update network-profiles to v0.0.3

--- Formula/network-profiles.rb
+++ Formula/network-profiles.rb
@@ -1,12 +1,12 @@
-class Networkprofiles < Formula
+class Network-profiles < Formula
   desc ""
   homepage "https://github.com/PanterSoft/network-profiles"
-  url "https://github.com/PanterSoft/network-profiles/releases/download/v0.0.2/network-profiles-v0.0.2.tar.gz"
-  sha256 "d735c2c41232f9e3beea6059dedc16ab69c448f5d0e767c6b5809378dfca1138"
+  url "https://github.com/PanterSoft/network-profiles/releases/download/v0.0.3/network-profiles-v0.0.3.tar.gz"
+  sha256 "fd91aea8bf1b231d96e927e398b11d39bdf2b6a510ae28915240b8ec1f05dcca"
   license "MIT"
-  version "v0.0.2"
+  version "v0.0.3"
 
   def install
-    bin.install "networkProfiles"
+    bin.install "network-profiles"
   end
 end
