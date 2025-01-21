df36fb6ee4df32336f4dbf432c43399155af63f9
Nico Mattes <mattesnico@gmail.com>
2025-01-21 17:56:40 +0100

    added rpiboot formula

---
+++ Formula/rpiboot.rb
@@ -0,0 +1,31 @@
+class Rpiboot < Formula
+  desc "Tool to boot Raspberry Pi devices over USB"
+  homepage "https://github.com/raspberrypi/usbboot"
+  url "https://github.com/raspberrypi/usbboot.git",
+      :using => :git,
+      :revision => "HEAD",
+      :shallow => false
+  version "1.0.0"
+
+  depends_on "libusb"
+  depends_on "pkg-config"
+
+  def install
+    system "git", "clone", "--recurse-submodules", "--shallow-submodules", "--depth=1", "https://github.com/raspberrypi/usbboot", "usbboot"
+    cd "usbboot" do
+      system "make"
+      bin.install "rpiboot"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To run rpiboot, you may need to use sudo:
+        sudo rpiboot
+    EOS
+  end
+
+  test do
+    system "#{bin}/rpiboot", "--help"
+  end
+end
