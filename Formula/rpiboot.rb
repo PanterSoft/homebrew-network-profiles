8955284a1891a1b8877d082f54100171aa0f46fb
Nico <96704103+PanterSoft@users.noreply.github.com>
2025-01-22 10:52:36 +0100

    updated rpiboot formula

--- Formula/rpiboot.rb
+++ Formula/rpiboot.rb
@@ -1,8 +1,8 @@
 class Rpiboot < Formula
   desc "Raspberry Pi USB booting code, moved from tools repository"
   homepage "https://github.com/raspberrypi/usbboot"
-  url "https://github.com/raspberrypi/usbboot/archive/refs/tags/20221215-105.tar.gz"
-  sha256 "2f02dbe9a88e9dfad5f05e513e1f30afd47b1575820f7c3b0965bbaa"
+  url "https://github.com/raspberrypi/usbboot/archive/refs/tags/20221215-105525.tar.gz"
+  sha256 "2f02dbe9a88e9dfad5f05e513e1f30afd47b1575820f7c3b09665dfefc45bbaa"
   license "Apache-2.0"
   version "20221215-105525"
 
