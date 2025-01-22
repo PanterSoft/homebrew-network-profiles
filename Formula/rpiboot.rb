45d58628be51068ce3b2fd74b22d64543505d08b
Nico Mattes <mattesnico@gmail.com>
2025-01-22 10:19:54 +0100

    re added dependencys

--- Formula/rpiboot.rb
+++ Formula/rpiboot.rb
@@ -1,10 +1,14 @@
 class Rpiboot < Formula
   desc "Raspberry Pi USB booting code, moved from tools repository"
   homepage "https://github.com/raspberrypi/usbboot"
-  url "https://github.com/raspberrypi/usbboot/archive/refs/tags/20221215-105525.tar.gz"
+  url "https://github.com/raspberrypi/usbboot/archive/refs/tags/20221215-105525.tar.gz",
+      :using => :homebrew_curl
   sha256 "2f02dbe9a88e9dfad5f05e513e1f30afd47b1575820f7c3b09665dfefc45bbaa"
   license "Apache-2.0"
   version "20221215-105525"
+
+  depends_on "libusb"
+  depends_on "pkg-config"
 
   def install
     system "make"
