6f5f8368b4ddf12e138b24758e43e598e85d66b8
Nico Mattes <mattesnico@gmail.com>
2025-01-29 09:46:04 +0100

    fixed rpiboot formula

--- Formula/rpiboot.rb
+++ Formula/rpiboot.rb
@@ -1,7 +1,7 @@
 class Rpiboot < Formula
   desc "Raspberry Pi USB booting code, moved from tools repository"
   homepage "https://github.com/raspberrypi/usbboot"
-  url "https://github.com/raspberrypi/usbboot/archive/refs/tags/20221215-105525.tar.gz"
+  url "https://github.com/raspberrypi/usbboot.git", using: :git, shallow: true
   sha256 "2f02dbe9a88e9dfad5f05e513e1f30afd47b1575820f7c3b09665dfefc45bbaa"
   license "Apache-2.0"
   version "20221215-105525"
@@ -10,14 +10,17 @@ class Rpiboot < Formula
   depends_on "pkg-config"
 
   def install
+    system "git", "submodule", "init"
+    system "git", "submodule", "update", "--recursive"
     system "make"
     bin.install "rpiboot"
+    prefix.install Dir["*"]
   end
 
   def caveats
     <<~EOS
       To run rpiboot, you may need to use sudo:
-        sudo rpiboot -d mass-storage-gadget64
+        sudo rpiboot -d #{prefix}/mass-storage-gadget64
     EOS
   end
 
--- README.md
+++ README.md
@@ -11,3 +11,13 @@
 |-----------------|-----------------|------------------------------|
 | :x: | NetworkProfiles | Manage network profiles inside your Menue Bar      |
 | :white_check_mark: | rpiboot         | Boot Raspberry Pi over USB (https://github.com/raspberrypi/usbboot)  |
+
+
+### Formula Descriptions
+
+#### Rpiboot
+
+If you want to flash a RPI5/CM5 use the command with the `-d` flag and the mass-storage-gadget64 directory. the correct command is shown when executing:
+```sh
+brew info rpiboot
+```
